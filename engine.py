5cbb133341e75d31edbf76006d859316ab80ed62
Maksymilian Pierchała <maksdfg123@gmail.com>
2021-01-21 20:52:53 +0100

    Added winner checking and undo moves

--- engine.py
+++ engine.py
@@ -1,8 +1,8 @@
-'''
-This part of code might be used for checking valid moves, checking board, minimax algorithm
-'''
 
 class State():
+    """
+    Main engine class used to checking valid moves, checking board, minimax algorithm. This class is mainly to get the game working.
+    """
     def __init__(self):
         
         # This is a represantation of a 3x3 tic tac toe board, dot '.' means blank space
@@ -14,31 +14,107 @@ class State():
         self.move_logs = []
         self.sign = 'x'
         self.message = {'x': "It is x's turn.", 'o': "It is o's turn."}
-        self.errors = {'place_error': "Place is already taken."}
-
+        self.errors = {'place_error': "Place is already taken.", 'undo_error':"No moves to undo."}
+        self.wins = {'x': 'Cross wins', 'o': 'Circle wins', 'draw': "It's a draw."}
+        self.winner = ''
+        
     def makeMove(self, pos, SQ_SIZE):
+        ''' Making a move is made by changing blank space in board to either 'x' or 'o' '''
         row = pos[1]//SQ_SIZE
         col = pos[0]//SQ_SIZE
         try:
             if self.board[row][col] == '.':
                 self.board[row][col] = self.sign
                 self.move_logs.append((row, col))
+                print(self.move_logs)
                 self.changeSign()
         except:
             self.errors['place_error']
 
     def changeSign(self):
+        ''' This fucntion is called when you have to change the player that is supposed to move. For example: after move, when undo. '''
         if self.sign == 'x':
             self.sign = 'o'
+            self.message[self.sign]
         else:
             self.sign = 'x'
+            self.message[self.sign]
         self.message[self.sign]
         self.x_to_move = not self.x_to_move
 
-    
+    def checkWinner(self):
+        ''' This function is checking if someone won in any way that is below. '''
+        if self.checkHorizontaly():
+            return True
+        elif self.checkVerticaly():
+            return True
+        elif self.checkDiagonally():
+            return True
+        elif self.isDraw():
+            return True
+            
+    def checkHorizontaly(self):
+        ''' Function used for checking if someone won by checking every row. '''
+        for r in self.board:
+            if len(set(r)) == 1 and '.' not in r: # Taking a list of signs in next rows and making it a set. !! This method is used in every win-checking function. !! 
+                self.winner = r[0]
+                return True
 
+    def checkVerticaly(self):
+        ''' Function used for checking if someone won by checking every column. ''' 
+        first_row = [item[0] for item in self.board]
+        second_row = [item[1] for item in self.board]
+        third_row = [item[2] for item in self.board]
 
+        if len(set(first_row)) == 1 and '.' not in first_row:
+            self.winner = self.board[0][0]
+            return True
+        if len(set(second_row)) == 1 and '.' not in second_row:
+            self.winner = self.board[0][1]
+            return True
+        if len(set(third_row)) == 1 and '.' not in third_row:
+            self.winner = self.board[0][2]
+            return True
+
+    def checkDiagonally(self):
+        ''' Function used to check winner of diagonalls '''
+        n = len(self.board)
+        # Left to right
+        l_to_r = [self.board[i][i] for i in range(len(self.board))]
+        r_to_l = [self.board[n-1-i][i] for i in range(n)]
         
+        if len(set(l_to_r)) == 1 and '.' not in l_to_r:
+            self.winner = self.board[0][0]
+            return True
+        elif len(set(r_to_l)) == 1 and '.' not in r_to_l:
+            self.winner = self.board[0][2]
+            return True
 
+    def isDraw(self):
+        """ Function used for checking if it is a draw """
+        if len(self.move_logs) == 9:
+            self.winner = 'draw'
+            return True
+        else:
+            return False
+            
+    def undo(self):
+        """ Function used for undoing moves """
+        if len(self.move_logs) == 0:
+            self.errors['undo_error']
+        else:
+            # n = len(self.move_logs)-1
+            n = self.move_logs.pop()
+            self.board[n[0]][n[1]] = '.'
+            self.x_to_move = not self.x_to_move
+            print(n)
+            print(self.board)
+
+    def canMove(self):
+        """ Function checking if next move is possible  """
+        if self.checkWinner():
+            return False
+        else:
+            return True
 
     
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ This file is responsible to take user input, draw things, display board etc.
 '''
  TODO In this app: add MiniMax algorithm, check for winner,
     display messages, create choice about type of game (PvP, PvPC),
-    ask for another game at the end, Best Of 3, undo move.
+    ask for another game at the end, Best Of 3.
 '''
 
 
@@ -15,24 +15,44 @@ from pygame import freetype
 # Initialize pygame dir
 pygame.init()
 
-# Define const parameters of our game 
+# Define constant parameters of our game 
 WIDTH, HEIGHT = 600,700
-DIMENSIONS = 3 # Number of cells used in bot x and y axis
+RECT_WIDTH, RECT_HEIGHT = 600, 100
+DIMENSIONS = 3 # Number of cells used in both x and y axis
 SQ_SIZE = (WIDTH) // DIMENSIONS # Size of a single square (here 200x200)
 FPS = 20
 white, black = (255, 255, 255), (0, 0, 0)
-IMGS = {}
-GAME_FONT = pygame.freetype.SysFont("arial", 20) 
+IMGS = {} 
+GAME_FONT = pygame.freetype.SysFont("arial", 32) 
 eng = engine.State()
 gameDisplay = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 def loadImgs():
     '''
-    Load images
+    Loads images
     '''
     IMGS['x'] = pygame.transform.scale(pygame.image.load('imgs/X.jpg'), (SQ_SIZE, SQ_SIZE))
     IMGS['o'] = pygame.transform.scale(pygame.image.load('imgs/o.svg'), (SQ_SIZE, SQ_SIZE))
+
+def displayMessages():
+    """
+    Displays errors, messages, winners at the bottom of the screen
+    """
+    # Display message taken from engine --
+    if eng.checkWinner():
+        text_surface, rect = GAME_FONT.render(eng.wins[eng.winner], (white))
+        drawWinnerLine()
+    else:
+        text_surface, rect = GAME_FONT.render(eng.message[eng.sign], (white))
+    gameDisplay.blit(text_surface, (220, 640))
+
+def drawWinnerLine():
+    """
+    Have no idea how to do it yet 
+    """
+    if eng.checkWinner():
+        pass
 
 def displayBoard():
     '''
@@ -44,26 +64,31 @@ def displayBoard():
         for c in range(len(eng.board[r])):
             if eng.board[c][r] != '.': 
                 gameDisplay.blit(IMGS[eng.board[c][r]], (r*SQ_SIZE, c*SQ_SIZE))
+            else:
+                pygame.draw.rect(gameDisplay, white,[SQ_SIZE*r, SQ_SIZE*c, SQ_SIZE, SQ_SIZE])
 
-    # Vertical lines
+    # Draw Vertical lines
     pygame.draw.line(gameDisplay, black, (SQ_SIZE, 0), (SQ_SIZE, HEIGHT-100), 5)
     pygame.draw.line(gameDisplay, black, (2*SQ_SIZE, 0), (2*SQ_SIZE, HEIGHT-100), 5)
 
-    # Horizontal lines
+    # Draw Horizontal lines
     pygame.draw.line(gameDisplay, black, (0, SQ_SIZE), (WIDTH, SQ_SIZE), 5)
     pygame.draw.line(gameDisplay, black, (0, 2*SQ_SIZE), (WIDTH, 2*SQ_SIZE), 5)
 
     # Draw message box
     pygame.draw.rect(gameDisplay, black, (0, 600, WIDTH, 100))
 
-    # Display message taken from engine --
-    text_surface, rect = GAME_FONT.render("Hello world!", (0,0,0))
+def state():
+    """
+    Display current game state
+    """
+    displayBoard()
+    displayMessages()
 
 def main():
     '''
     Main function used to run the whole program
     '''
-
     running = True
     clock = pygame.time.Clock()
     gameDisplay.fill(white)
@@ -72,12 +97,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.MOUSEBUTTONDOWN: 
                 pos = pygame.mouse.get_pos()
-                eng.makeMove(pos, SQ_SIZE)
-        displayBoard()
+                if eng.canMove(): 
+                    eng.makeMove(pos, SQ_SIZE) 
+            elif event.type == pygame.KEYDOWN: 
+                if event.key == pygame.K_z: 
+                    eng.undo()
+                    eng.changeSign()
+
+        state()
         clock.tick(FPS)
-        pygame.display.flip()
+        pygame.display.update()
 
 
 # Starting program
